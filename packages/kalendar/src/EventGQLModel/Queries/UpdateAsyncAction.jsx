7a9dd940f5ae8eba9cc89e1f06b7830acd48a9e2
Matyas Kopecek <matykopecek@gmail.com>
2026-05-12 18:44:41 +0200

    upravaUpdateAsyncAction(nefunkcni)

--- packages/kalendar/src/EventGQLModel/Queries/UpdateAsyncAction.jsx
+++ packages/kalendar/src/EventGQLModel/Queries/UpdateAsyncAction.jsx
@@ -1,56 +1,11 @@
 import { createQueryStrLazy } from "@hrbolek/uoisfrontend-gql-shared";
-import { LargeFragment } from "./Fragments";
 import { createAsyncGraphQLAction2 } from "../../../../dynamic/src/Core/createAsyncGraphQLAction2";
 import { reduceToFirstEntity, updateItemsFromGraphQLResult } from "../../../../dynamic/src/Store";
 
 const UpdateMutationStr = `
-mutation eventUpdate($event_id: UUID!, $event_lastchange: DateTime!, $event_name: String, $event_nameEn: String, $event_description: String, $event_startdate: DateTime, $event_enddate: DateTime) {
-  eventUpdate(event: {id: $event_id, lastchange: $event_lastchange, name: $event_name, nameEn: $event_nameEn, description: $event_description, startdate: $event_startdate, enddate: $event_enddate}) {
-  ... on EventGQLModel { ...Event }
-  ... on EventGQLModelUpdateError { ...Error }
-}
-}
-
-fragment User on UserGQLModel {
-      __typename
-      id
-      studies { id }
-      invitations { id }
-      lastchange
-      created
-      createdbyId
-      changedbyId
-      rbacobjectId
-      createdby { id }
-      changedby { id }
-      rbacobject { id }
-      name
-      givenname
-      middlename
-      email
-      firstname
-      surname
-      valid
-      startdate
-      enddate
-      typeId
-      memberships { id }
-      roles { id }
-      isThisMe
-      rolesOn { id }
-      gdpr
-      fullname
-      memberOf { id }
-    }
-
-fragment RBACObject on RBACObjectGQLModel {
-      __typename
-      id
-      roles { id }
-      currentUserRoles { id }
-    }
-
-fragment Facility on FacilityGQLModel {
+mutation eventUpdate($id: UUID!, $lastchange: DateTime!, $event_name: String, $event_nameEn: String, $event_description: String, $event_startdate: DateTime, $event_enddate: DateTime) {
+  eventUpdate(event: {id: $id, lastchange: $lastchange, name: $event_name, nameEn: $event_nameEn, description: $event_description, startdate: $event_startdate, enddate: $event_enddate}) {
+    ... on EventGQLModel { 
       __typename
       id
       lastchange
@@ -58,132 +13,33 @@ fragment Facility on FacilityGQLModel {
       createdbyId
       changedbyId
       rbacobjectId
-      createdby { id }
-      changedby { id }
-      rbacobject { id }
-      path
-      name
-      nameEn
-      label
-      startdate
-      enddate
-      address
-      valid
-      capacity
-      geometry
-      geolocation
-      reservations { id }
-      groupId
-      facilitytypeId
-      masterFacilityId
-      type { id }
-      masterFacility { id }
-      masterFacilities { id }
-      subFacilities { id }
-      group { id }
-    }
-
-fragment EventFacilityReservation on EventFacilityReservationGQLModel {
-      __typename
-      id
-      lastchange
-      created
-      createdbyId
-      changedbyId
-      rbacobjectId
-      createdby { id }
-      changedby { id }
-      rbacobject { id }
-      eventId
-      event { id }
-      facilityId
-      facility { id }
-      stateId
-      state { id }
-    }
-
-fragment EventType on EventTypeGQLModel {
-      __typename
-      id
-      lastchange
-      created
-      createdbyId
-      changedbyId
-      rbacobjectId
-      createdby { id }
-      changedby { id }
-      rbacobject { id }
       path
       name
       nameEn
       description
-      parentId
-      parent { id }
-      children { id }
-      events { id }
+      startdate
+      enddate
+      duration_raw
+      valid
+      place
+      facilityId
+      mastereventId
+      typeId
     }
-
-fragment EventInvitation on EventInvitationGQLModel {
+    ... on EventGQLModelUpdateError { 
       __typename
-      id
-      lastchange
-      created
-      createdbyId
-      changedbyId
-      rbacobjectId
-      createdby { id }
-      changedby { id }
-      rbacobject { id }
-      eventId
-      userId
-      stateId
-      event { id }
-      user { id }
-      state { id }
+      msg
+      failed
+      code
     }
-
-fragment Event on EventGQLModel {
-    __typename
-    id
-    lastchange
-    created
-    createdbyId
-    changedbyId
-    rbacobjectId
-    createdby { ...User }
-    changedby { ...User }
-    rbacobject { ...RBACObject }
-    path
-    name
-    nameEn
-    description
-    startdate
-    enddate
-    duration_raw
-    valid
-    place
-    facilityId
-    facility { ...Facility }
-    facilityReservations { ...EventFacilityReservation }
-    mastereventId
-    masterevent { id }
-    subevents { id }
-    typeId
-    type { ...EventType }
-    userInvitations { ...EventInvitation }
   }
+}
+`;
 
-fragment Error on EventGQLModelUpdateError {
-    __typename
-    Entity { ...Event }
-    msg
-    failed
-    code
-    location
-    input
-  }
-`
+const UpdateMutation = createQueryStrLazy(`${UpdateMutationStr}`);
 
-const UpdateMutation = createQueryStrLazy(`${UpdateMutationStr}`)
-export const UpdateAsyncAction = createAsyncGraphQLAction2(UpdateMutation, 
-    updateItemsFromGraphQLResult, reduceToFirstEntity)
+export const UpdateAsyncAction = createAsyncGraphQLAction2(
+  UpdateMutation, 
+  updateItemsFromGraphQLResult, 
+  reduceToFirstEntity
+);
